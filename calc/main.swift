940b8106b49bb3e568bcf5c6576201809c1b6759
HanThiSweNaing <48665712+HanThiSweNaing@users.noreply.github.com>
2019-03-28 13:25:06 +1100

    Update main.swift

--- calc/main.swift
+++ calc/main.swift
@@ -8,7 +8,81 @@
 
 import Foundation
 
-var args = ProcessInfo.processInfo.arguments
-args.removeFirst() // remove the name of the program
+var numberOnScreen:Double = 0;
+    var previousNumber:Double = 0;
+    var performingMath = false;
+    var operation = 0;
 
-print(Int(args[0])!)
+        if performingMath == true
+        {
+            label.text = String(sender.tag-1);
+            numberOnScreen = Double(label.text!)!
+            performingMath = false
+        }
+        else
+        {
+       //display the number that is pressed
+        label.text = label.text! + String(sender.tag-1)
+        numberOnScreen = Double(label.text!)!
+        }
+    
+        if label.text != "" && sender.tag != 11 && sender.tag != 16
+        {
+            previousNumber = Double(label.text!)!
+            if sender.tag == 12 // /
+            {
+                label.text = "/";
+            }
+            else if sender.tag == 13 // X
+            {
+                label.text = "x";
+            }
+            else if sender.tag == 14 // -
+            {
+                label.text = "-";
+            }
+            else if sender.tag == 15  //+
+            {
+                //display the + sign
+                label.text = "+";
+            }
+        
+            operation = sender.tag;
+            performingMath = true;
+        }
+        else if sender.tag == 16
+        {
+            if operation == 15
+            {
+                label.text = String(previousNumber + numberOnScreen)
+            }
+            if operation == 14
+            {
+                label.text = String(previousNumber - numberOnScreen)
+            }
+            if operation == 13
+            {
+                label.text = String(previousNumber * numberOnScreen)
+            }
+            if operation == 12
+            {
+                label.text = String(previousNumber / numberOnScreen)
+            }
+        }
+        else if sender.tag == 11
+        {
+            label.text="";
+            previousNumber = 0;
+            numberOnScreen = 0;
+            operation = 0;
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+
+
